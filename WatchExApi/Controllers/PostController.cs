93a024430dfbb02c4b04960c111f9ac9ca2098c4
Devin Miller <devinmiller@codeonthebrain.com>
2019-02-25 20:06:28 -0700

    Initial commit.

---
+++ WatchExApi/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wex.Context;
+using Wex.Context.Models;
+
+namespace WatchExApi.Controllers
+{
+    [ODataRoutePrefix("posts")]
+    public class PostController : ODataController
+    {
+        private readonly WexContext _context;
+
+        public PostController(WexContext context)
+        {
+            _context = context;
+        }
+
+        [EnableQuery]
+        [ODataRoute]
+        public IActionResult Get()
+        {
+            return Ok(_context.Posts.Include(x => x.Images).AsQueryable());
+        }
+    }
+}
